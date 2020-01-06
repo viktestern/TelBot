5fcb6006488f9b47e0fbfc2d19a08e3fa5193e66
vikreator <sabvicant@gmail.com>
2020-01-06 20:01:15 +0300

    changes for first server itteration

--- server.py
+++ server.py
@@ -14,11 +14,11 @@ from middlewares import AccessMiddleware
 logging.basicConfig(level=logging.INFO)
 
 API_TOKEN = os.getenv("TELEGRAM_API_TOKEN")
-PROXY_URL = os.getenv("TELEGRAM_PROXY_URL")
-PROXY_AUTH = aiohttp.BasicAuth(
-    login=os.getenv("TELEGRAM_PROXY_LOGIN"),
-    password=os.getenv("TELEGRAM_PROXY_PASSWORD")
-)
+#PROXY_URL = os.getenv("TELEGRAM_PROXY_URL")
+#PROXY_AUTH = aiohttp.BasicAuth(
+#    login=os.getenv("TELEGRAM_PROXY_LOGIN"),
+#    password=os.getenv("TELEGRAM_PROXY_PASSWORD")
+#)
 ACCESS_ID = os.getenv("TELEGRAM_ACCESS_ID")
 
 bot = Bot(token=API_TOKEN, proxy=PROXY_URL, proxy_auth=PROXY_AUTH)
